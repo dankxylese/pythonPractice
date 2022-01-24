ab908c1448ed39cf498d501404f3e33aabc3db52
dankxylese <contact@darkxylese.com>
2022-01-24 11:35:11 +0000

    Add formatted strings to input.py

---
+++ variables/input.py
@@ -0,0 +1,5 @@
+age = int(input("What is your age \n"))  # without casting to an int, it saves as an str
+print("Your age is", age, type(age))
+print(f"You are {age + 1} years old next year")  # f = formatted string
+
+
