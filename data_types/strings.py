d5e5252d1130cd4fec610ab4733631d9707b7199
dankxylese <contact@darkxylese.com>
2022-01-24 12:33:40 +0000

    Completed tast in strings, added more to input

--- data_types/strings.py
+++ data_types/strings.py
@@ -1,10 +1,24 @@
-print("hello world")
-print('hello world')
+#print("hello world")
 
 # both are strings
 
-print("hello world"[1])  # strings are lists of characters. Access last char with [-1]
-print(len("hello world"))  # length of a string
+#print("hello world"[1])  # strings are lists of characters. Access last char with [-1]
+#print(len("hello world"))  # length of a string
 
-print("Bibhutibhushan Bandyopadhyay"[-29])  # bounds can go over, they will be ignored
+#print("Bibhutibhushan Bandyopadhyay"[-29].strip())  # bounds can go over, they will be ignored
+
+hi = "Hello World"
+print(hi
+      .replace(" ", "")
+      .isalpha())
+print(hi
+      .lower()
+      .islower())
+print(hi
+      .upper()
+      .isupper())
+print(hi
+      .endswith("d"))
+print(hi
+      .startswith("H"))
 
