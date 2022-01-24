398ecf0d8a2f020c23a1ef3a15788f395c8561cc
dankxylese <contact@darkxylese.com>
2022-01-24 15:27:35 +0000

    Add frozen set to sets.py

--- collections/sets.py
+++ collections/sets.py
@@ -14,3 +14,13 @@ pokemon.add("Bulbasaur")
 print(pokemon)
 # can't have multiples of items
 
+lst = [1, 2, 3, 1, 3, 1, 3, 2, 3, 3]
+print(lst)
+print(set(lst))
+# can remove duplicates by casting to a set
+
+#Frozen set
+
+x = frozenset(["let", "it", "go"])
+print(x)
+# immutable^
